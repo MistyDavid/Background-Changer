260394a5d84281b3ecefbed7e5c0af803e9e2906
MistyDavid <160311012+MistyDavid@users.noreply.github.com>
2024-03-14 10:37:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// ".first" vine folosit pentru CSS, ".js-first" pentru js
-// Se face asa separation of concern. Clasele care sunt cu prefixul "js" stii ca e nevoie de ele in javascript.
+
 
 const firstNumber = document.querySelector(".js-first"); 
 const secondNumber = document.querySelector(".js-second");
 const tirthNumber = document.querySelector(".js-tirth");
 const btnChange = document.querySelector(".js-btnChange");
 
-const MAGIC_NUMBER = 255; // dai un nume la variabile care explica ce-i cu valoarea asta de 255
+const MAGIC_NUMBER = 255;
 
 function getRandomNumber(){
 	return Math.floor(Math.random() * MAGIC_NUMBER);
